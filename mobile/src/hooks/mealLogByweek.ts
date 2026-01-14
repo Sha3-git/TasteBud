41d46b029eafa0f08adb3da36cadc719e4acd860
shema <dabirishema@gmail.com>
2026-01-13 23:50:39 -0600

    notes

--- mobile/src/hooks/mealLogByweek.ts
+++ mobile/src/hooks/mealLogByweek.ts
@@ -69,7 +69,7 @@ export function getMealLogByWeek(date: string, userId: string) {
         const mealRes = await mealLogService.getMealLogByWeek({ date, userId });
         const mealLogs = mealRes.data;
         console.log(mealLogs)
-        mealLogService.forEach((meal) =>{})
+        //mealLogService.forEach((meal) =>{})
         const meals: Meal[] = mealLogs.map((log: any) => ({
           id: log._id,
           name: log.mealName,
--- server/routes/brandedFoodRoute.js
+++ server/routes/brandedFoodRoute.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/brandedFoodController");
 
-router.get("/search", controller.searchFoods);
+router.get("/search", /*(--> insert auth middleware here)*/ controller.searchFoods);
 
 module.exports = router;
