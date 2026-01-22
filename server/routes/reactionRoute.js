f5774433dae6cf31271d0ff5ba8d07331990137e
Mazen Abid <mazenmeezo8@gmail.com>
2026-01-22 10:50:53 -0600

    fixed the  GET /reactions route and fix mealLogId typo in controller

--- server/routes/reactionRoute.js
+++ server/routes/reactionRoute.js
@@ -4,7 +4,8 @@ const router = express.Router();
 const reactionController = require("../controllers/reactionController")
 
 router.get("/daily", reactionController.getReactionByDay);
-router.post("/create", reactionController.createReaction)
+router.get("/", reactionController.getReaction);
+router.post("/create", reactionController.createReaction);
 
 router.put("/update/:id", reactionController.updateReaction);
 router.delete("/delete/:id", reactionController.deleteReaction);
